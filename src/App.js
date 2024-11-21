d047f186f8d0a0070a8cfa5e77080976d5d9e296
zpandeva <zoya>
2024-11-21 12:54:13 +0200

    change somthing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code> TTTTT src/App.js</code> and save to reload.
+          Edit <code> Zoya's src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
